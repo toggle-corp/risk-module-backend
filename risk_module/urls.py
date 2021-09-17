608f22ff933384528e66ad3635fb387fff23ad3d
k9845 <khatririshi2430@gmail.com>
2021-09-17 11:26:22 +0545

    Add production script

--- risk_module/urls.py
+++ risk_module/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.conf.urls import url, include, static
 from django.urls import path
+from django.conf import settings
+
 from earthquake.views import EarthquakeViewSet
 
 from rest_framework import routers
@@ -28,3 +30,4 @@ urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
